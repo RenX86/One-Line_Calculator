776fd392201a1a0e2b9e7fd415191dc6e20e5c13
RenX86 <galactusruler@gmail.com>
2025-01-02 11:26:21 +0530

    Update

--- Calulator.py
+++ Calulator.py
@@ -1,3 +1,3 @@
-CAUTION = " eval is risky funtion, it can run any code, so be careful, use at your own risk"
+CAUTION = " eval is risky, it can run any code, so be careful, use at your own risk"
 
 # while True: print(eval(input(">>>")))
